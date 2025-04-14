b453b7d6959ea7314757189f716914026c46fb0c
Marko Myllynen <myllynen@redhat.com>
2025-04-14 18:39:45 +0300

    win_remote_winrm.ps1: don't enable Basic-over-HTTPS by default

--- win_remote_winrm.ps1
+++ win_remote_winrm.ps1
@@ -1,14 +1,18 @@
-# Configure and enable Basic-over-HTTPS WinRM on Windows 2016+
-# Configure and enable NTLM-over-HTTP/S WinRM for non-Administrator
+# Configure WinRM/PSRP encrypted connection
+# Allow access for a non-Administrator user
 
 # This should be commented out in case profiles have been already setup
 Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
 
 Start-Service -Name WinRM
 Set-Service -Name WinRM -StartupType Automatic
+Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $false
+Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $false
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 
-# Allow NTLM for non-Administrator
+# Allow NTLM also for a non-Administrator in Administrators
 $user = 'winrm'
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name LocalAccountTokenFilterPolicy -Value 1
 $sid = (New-Object -TypeName System.Security.Principal.NTAccount -ArgumentList $user).Translate([System.Security.Principal.SecurityIdentifier])
 $sddl = (Get-Item -Path WSMan:\localhost\Service\RootSDDL).Value
 $sd = New-Object -TypeName System.Security.AccessControl.CommonSecurityDescriptor -ArgumentList $false, $false, $sddl
@@ -22,30 +26,50 @@ $sd.DiscretionaryAcl.AddAccess(
 $sddl = $sd.GetSddlForm([System.Security.AccessControl.AccessControlSections]::All)
 Set-Item -Path WSMan:\localhost\Service\RootSDDL -Value $sddl -Force
 
-# Basic-over-HTTPS
-if (!(Get-NetFirewallRule -Name WINRM-HTTPS-In-TCP -ErrorAction SilentlyContinue | Select-Object -Property Name, Enabled)) {
+$rule = Get-NetFirewallRule -Name WINRM-HTTP-In-TCP -ErrorAction SilentlyContinue
+if (-not $rule) {
   New-NetFirewallRule `
     -Enabled True `
-    -Name WINRM-HTTPS-In-TCP `
-    -DisplayName 'Windows Remote Management (HTTPS-In)' `
-    -Description 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]' `
+    -Name WINRM-HTTP-In-TCP `
+    -DisplayName 'Windows Remote Management (HTTP-In)' `
+    -Description 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5985]' `
+    -DisplayGroup 'Windows Remote Management'
     -Group '@FirewallAPI.dll,-30267' `
-    -LocalPort 5986 `
+    -LocalPort 5985 `
     -Action Allow `
     -Direction Inbound `
     -Protocol TCP `
     -Profile @('Domain', 'Private')
 }
+if ($rule -and ($rule.Enabled -ne $true -or $rule.Action -ne 'Allow')) {
+  Set-NetFirewallRule -Name WINRM-HTTP-In-TCP -Enabled True -Profile @('Domain', 'Private') -Action Allow
+}
 
-$friendlyName = 'WinRM over HTTPS'
-$cert = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My `
-          -DnsName $env:COMPUTERNAME -NotAfter (get-date).AddYears(10) `
-          -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider' `
-          -KeyLength 4096
-$cert.FriendlyName = $friendlyName
-New-Item -Path WSMan:\localhost\Listener -Transport HTTPS `
-  -Address * -CertificateThumbPrint $cert.Thumbprint -Force
-
-Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $false
-Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $true
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+# WinRM/HTTPS
+#$friendlyName = 'WinRM over HTTPS'
+#$cert = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My `
+#          -DnsName $env:COMPUTERNAME -NotAfter (get-date).AddYears(10) `
+#          -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider' `
+#          -KeyLength 4096
+#$cert.FriendlyName = $friendlyName
+#Set-WSManInstance -ResourceURI winrm/config/Listener `
+#  -SelectorSet @{Address='*';Transport='HTTPS'} `
+#  -ValueSet @{CertificateThumbprint=$cert.Thumbprint;Enabled=$true}
+#$rule = Get-NetFirewallRule -Name WINRM-HTTPS-In-TCP -ErrorAction SilentlyContinue
+#if (-not $rule) {
+#  New-NetFirewallRule `
+#    -Enabled True `
+#    -Name WINRM-HTTPS-In-TCP `
+#    -DisplayName 'Windows Remote Management (HTTPS-In)' `
+#    -Description 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]' `
+#    -DisplayGroup 'Windows Remote Management'
+#    -Group '@FirewallAPI.dll,-30267' `
+#    -LocalPort 5986 `
+#    -Action Allow `
+#    -Direction Inbound `
+#    -Protocol TCP `
+#    -Profile @('Domain', 'Private')
+#}
+#if ($rule -and ($rule.Enabled -ne $true -or $rule.Action -ne 'Allow')) {
+#  Set-NetFirewallRule -Name WINRM-HTTPS-In-TCP -Enabled True -Profile @('Domain', 'Private') -Action Allow
+#}
